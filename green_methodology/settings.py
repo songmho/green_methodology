5997c7ed68a0ab2f0313b224af9a855dec09f057
songmho <songmho@gmail.com>
2019-12-27 09:58:08 +0900

    Finish setting for Heroku

--- green_methodology/settings.py
+++ green_methodology/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9p+h&o3)0_jw3h-o7v$&9nlo%o=vh$(4imu_!thln@gup6wot-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9p+h&o3)0_jw3h-o7v$&9nlo%o=vh$(4imu_!thln@gup6wot-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
